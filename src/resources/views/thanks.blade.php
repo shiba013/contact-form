05eed1570b305e85f07ded6ec71262e8e5d3dae7
reimi <r.yamada013@icloud.com>
2025-02-13 00:35:22 +0900

    編集済み

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    @extends('layouts.app')
+
+    @section('css')
+    <link rel="stylesheet" href="{{asset('css/thanks.css')}}">
+    @endsection
+
+    @section('content')
+    <div class="thanks__content">
+            <div class="thanks__heading">
+                <h2>お問い合わせありがとうございます</h2>
+            </div>
+        </div>
+    @endsection
+</head>
+<body>
+   
+</body>
+</html>
